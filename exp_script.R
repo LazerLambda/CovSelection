40e89c3764f57e570431f70364df714b77d9a12c
Philipp Koch <PhillKoch@protonmail.com>
2023-08-25 02:12:01 +0200

    Add python script

---
+++ exp_script.R
@@ -0,0 +1,109 @@
+library(huge)
+library(logger)
+library(peakRAM)
+
+source("./utils.R")
+source("./MB_DGF.R")
+
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+# 
+n <- as.numeric(args[1])
+dims <- as.numeric(unlist(strsplit(args[2], ',')))
+graph <- args[3]
+method <- args[4]
+selector <- args[5]
+tracking_file <- args[6]
+seed <- args[7]
+set.seed(seed)
+
+
+if(method != 'mb' | selector != 'ebic') {
+  df_len <- length(dims)
+  tracking <- data.frame(
+    seed = rep(NA, df_len),
+    graph = rep(NA, df_len),
+    dim = rep(NA, df_len),
+    method = rep(NA, df_len),
+    selector = rep(NA, df_len),
+    elapsed_time_model = rep(NA, df_len),
+    total_ram_model = rep(NA, df_len),
+    peak_ram_model = rep(NA, df_len),
+    elapsed_time_best = rep(NA, df_len),
+    total_ram_best = rep(NA, df_len),
+    peak_ram_best = rep(NA, df_len),
+    f1 = rep(NA, df_len),
+    precision = rep(NA, df_len),
+    recall = rep(NA, df_len),
+    hamming_dist = rep(NA, df_len),
+    best = rep(NA, df_len)
+  )
+  print(dims)
+
+  counter <- 1
+
+  for (d in dims){
+    if(graph !=  "MB") {
+      gen_data <- huge.generator(
+        n = n,
+        d = d,
+        graph = graph,
+      )
+    } else {
+      gen_data <- mb_gen(
+        n = n,
+        d = d
+      )
+    }
+    
+    # TODO: Track time for one estimation?
+    
+    # Model Computation
+    res_model <- peakRAM({
+      model <- huge(
+        x = gen_data$data,
+        method = method,
+        sym = 'or')
+      end_time <- Sys.time()
+    })
+    
+    # Model Selection
+    res_best <- peakRAM({
+      best <- huge.select(model, criterion = selector)
+    })
+    
+    metrics <- get_metrics(
+      as(best$refit, "dgCMatrix"),
+      as(gen_data$theta, "dsCMatrix"))
+    
+    tracking[counter, ]$seed <- seed
+    tracking[counter, ]$graph <- graph
+    tracking[counter, ]$method <- method
+    tracking[counter, ]$dim <- d
+    tracking[counter, ]$selector <- selector
+    tracking[counter, ]$elapsed_time_model <- res_model$Elapsed_Time_sec
+    tracking[counter, ]$total_ram_model <- res_model$Total_RAM_Used_MiB
+    tracking[counter, ]$peak_ram_model <- res_model$Peak_RAM_Used_MiB
+    tracking[counter, ]$elapsed_time_best <- res_best$Elapsed_Time_sec
+    tracking[counter, ]$total_ram_best <- res_best$Total_RAM_Used_MiB
+    tracking[counter, ]$peak_ram_best <- res_best$Peak_RAM_Used_MiB
+    tracking[counter, ]$f1 <- metrics$f1
+    tracking[counter, ]$precision <- metrics$precision
+    tracking[counter, ]$recall <- metrics$recall
+    tracking[counter, ]$hamming_dist <- metrics$hamming_dist
+    tracking[counter, ]$best <- 0
+    
+    counter <- counter + 1
+  }
+
+  # tracker <- read.csv(tracking_file)
+  if (!file.exists("test.csv")) {
+    write.csv(tracking, "test.csv", row.names = FALSE)
+  } else {
+    curr <- read.csv("test.csv")
+    curr <- rbind(curr, tracking)
+    write.csv(curr, "test.csv", row.names = FALSE) 
+  }
+
+}
---
+++ run_script.py
@@ -0,0 +1,41 @@
+
+import argparse
+import datetime
+import os
+import logging
+
+parser = argparse.ArgumentParser(
+    prog='Experiment Wrapper',
+    description='Call the R-experiment script.',
+    epilog='')
+
+parser.add_argument(
+    '-f',
+    '--csv-out',
+    type=str,
+    required=False,
+    default=('csv-out' + str(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.csv'))
+    )
+args = parser.parse_args()
+
+target_file: str = args.csv_out
+print(target_file)
+n: str = "100"
+dims: str = "100,250" #,500,750,1000,1500"
+graphs: list = ["hub", "cluster", "band", "scale-free", "MB"]
+methods: list = ["glasso", "mb"]
+selectors: list = ['stars', 'ebic']
+seeds: list = [123, 42]
+
+total: int = len(dims.split(',')) * len(graphs) * len(methods) * len(selectors) * len(seeds)
+counter: int = 0
+for seed in seeds:
+    for graph in graphs:
+        for method in methods:
+            for selector in selectors:
+                logging.info(f"{counter}/{total}")
+                command_str: str = f"Rscript exp_script.R {n} {dims} {graph} {method} {selector} {target_file} {seed}"
+                stream = os.popen(command_str)
+                output = stream.read()
+                print(output)
+                counter += 1
