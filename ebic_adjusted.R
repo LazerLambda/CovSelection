4c77f10c57f9dd2e9bc21222eb47469317443e85
Philipp Koch <PhillKoch@protonmail.com>
2023-08-26 01:57:23 +0200

    Add adjusted ebic to avoid preference for total sparsity

--- ebic_adjusted.R
+++ ebic_adjusted.R
@@ -3,20 +3,12 @@
 ebic_corrected <- function(est,d, n, ebic.gamma = 0.5){
   if(est$method == "glasso")
   {
-    # if (min(est$sparsity) == 0){
-    #   indices <- which(!est$sparsity == 0)
-    #   est$ebic.score = -n*est$loglik[indices] + log(n)*est$df[indices] + 4*ebic.gamma*log(d)*est$df[indices]
-    #   est$opt.index = which.min(est$ebic.score[indices])
-    # } else {
-    #   
-    #   est$opt.index = which.min(est$ebic.score[est$ebic.score != 0]) 
-    # }
     est$ebic.score = -n*est$loglik + log(n)*est$df + 4*ebic.gamma*log(d)*est$df
-    est$opt.index = which.min(est$ebic.score[est$ebic.score != n * d])
-    print(est$loglik)
-    print(n * d)
-    print(est$ebic.score[est$ebic.score != n * d])
-    print(est$opt.index)
+    if (TRUE %in% (est$sparsity == 0)) {
+      ind <- which(est$sparsity == 0)
+      est$ebic.score[[ind]] <- max(est$ebic.score)
+    }
+    est$opt.index = which.min(est$ebic.score)
     est$refit = est$path[[est$opt.index]]
     est$opt.icov = est$icov[[est$opt.index]]
     if(est$cov.output)
