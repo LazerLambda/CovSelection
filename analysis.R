180b90da8ffffc25470b8d95a743ad7c1bac1100
Philipp Koch <PhillKoch@protonmail.com>
2023-09-01 14:27:07 +0200

    Add hamming distance to analysis

--- analysis.R
+++ analysis.R
@@ -6,6 +6,14 @@ library(ggplot2)
 
 # PREPARE DATA
 
+# install.packages('Metrics')
+# install.packages('testthat')
+# install.packages('checkmate')
+# install.packages('logger')
+# install.packages('huge')
+# install.packages('peakRAM')
+# install.packages('MASS')
+
 setwd("/home/philko/Documents/Uni/SoSe23/SparseAndNetwork")
 data <- read.csv('./data/data.csv')
 data$name <- apply( data[ , c('method', 'selector')] , 1 , paste , collapse = "-" )
@@ -65,7 +73,7 @@ get_title <- function(graph_name, short = F) {
   if (graph_name == "MB" & short) return("MB-DGP")
 }
 
-plot_models <- function(df, col_name) {
+plot_models <- function(df, col_name, df_unsorted = NULL) {
   plot <- ggplot(data=df, aes_string(x="dim", y=paste(col_name, "mean", sep="_"), group="name", fill = "name") ) +
     geom_line(aes(col = name)) +
     scale_color_manual(
@@ -78,7 +86,7 @@ plot_models <- function(df, col_name) {
         "glasso-stars" = "#5D8AA8", # air force blue
         "glasso-ebic" = "#E52B50", # amararanth
         "mb-stars" = "#ED872D")) + # cadmium orange
-    geom_point() +
+    # {if(is.null(df_unsorted)) geom_point() + }
     ylab("") +
     xlab("") +
     geom_ribbon(
@@ -96,6 +104,16 @@ plot_models <- function(df, col_name) {
     } else {
       xlab("")}
     }
+  if (!is.null(df_unsorted)) {
+    plot <- plot +
+      geom_point(
+        data=df_unsorted,
+        aes_string(
+          x="dim",
+          y=col_name,
+          group="name",
+          col="name"))
+  }
   return(plot)
 }
 
@@ -114,13 +132,16 @@ for (g in graphs) {
   data_tmp <- data_grouped %>%
     filter(graph == g)
   data_tmp <- na.omit(data_tmp)
-  plot1 <- plot_models(data_tmp, "f1") +
+  data_uns <- data %>%
+    filter(graph == g)
+  data_uns <- na.omit(data_uns)
+  plot1 <- plot_models(data_tmp, "f1", data_uns) +
     ggtitle("F1")
 
-  plot2 <- plot_models(data_tmp, "precision") +
+  plot2 <- plot_models(data_tmp, "precision", data_uns) +
     ggtitle("Precision")
 
-  plot3 <- plot_models(data_tmp, "recall") +
+  plot3 <- plot_models(data_tmp, "recall", data_uns) +
     ggtitle("Recall")
 
   plt <- grid.arrange(
@@ -132,7 +153,7 @@ for (g in graphs) {
     plt,
     file=paste("./figures/", g, "_perf", ".pdf", sep = ""),
     width = 22,
-    height = 10,
+    height = 8,
     units = "cm")
   plots_perf[[i]] <- plt
   i <- i + 1
@@ -151,10 +172,13 @@ for (g in graphs) {
   data_tmp <- data_grouped %>%
     filter(graph == g)
   data_tmp <- na.omit(data_tmp)
-  plot1 <- plot_models(data_tmp, "elapsed_time_model") +
+  data_uns <- data %>%
+    filter(graph == g)
+  data_uns <- na.omit(data_uns)
+  plot1 <- plot_models(data_tmp, "elapsed_time_model", data_uns) +
     ylab("Time (in Seconds)") +
     ggtitle("Model Estimation")
-  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "elapsed_time_best") +
+  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "elapsed_time_best", data_uns) +
     ggtitle("Model Selection")
 
   plt <- grid.arrange(
@@ -181,8 +205,11 @@ for (g in graphs) {
   data_tmp <- data_grouped %>%
     filter(graph == g)
   data_tmp <- na.omit(data_tmp)
-  plot1 <- plot_models(data_tmp, "elapsed_time_model") + ggtitle(get_title(g, T)) + coord_cartesian(ylim = c(0, 80))
-  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "elapsed_time_best") + coord_cartesian(ylim = c(0, 1500))
+  data_uns <- data %>%
+    filter(graph == g)
+  data_uns <- na.omit(data_uns)
+  plot1 <- plot_models(data_tmp, "elapsed_time_model", data_uns) + ggtitle(get_title(g, T)) + coord_cartesian(ylim = c(0, 80))
+  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "elapsed_time_best", data_uns) + coord_cartesian(ylim = c(0, 1500)) + ggtitle("")
   
   plots_time_mod[[i]] <- plot1
   plots_time_bst[[i]] <- plot2
@@ -210,10 +237,16 @@ for (g in graphs) {
   data_tmp <- data_grouped %>%
     filter(graph == g)
   data_tmp <- na.omit(data_tmp)
-  plot1 <- plot_models(data_tmp, "peak_ram_model") +
+  data_uns <- data %>%
+    filter(graph == g)
+  data_uns <- na.omit(data_uns)
+  plot1 <- plot_models(data_tmp, "peak_ram_model", data_uns) +
     ylab("Memory (in MiB)") +
     ggtitle("Model Estimation")
-  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "peak_ram_best") +
+  plot2 <- plot_models(
+    data_tmp[!(data_tmp$name == "glasso-ebic"), ],
+    "peak_ram_best",
+    data_uns[!(data_uns$name == "glasso-ebic"), ]) +
     ggtitle("Model Selection")
 
 
@@ -240,19 +273,132 @@ for (g in graphs) {
   data_tmp <- data_grouped %>%
     filter(graph == g)
   data_tmp <- na.omit(data_tmp)
-  plot1 <- plot_models(data_tmp, "peak_ram_model") + ggtitle(get_title(g, T))
-  plot2 <- plot_models(data_tmp[!(data_tmp$name == "glasso-ebic"), ], "peak_ram_best")
+  data_uns <- data %>%
+    filter(graph == g)
+  data_uns <- na.omit(data_uns)
+  plot1 <- plot_models(data_tmp, "peak_ram_model", data_uns) + ggtitle(get_title(g, T))
+  plot2 <- plot_models(
+    data_tmp[!(data_tmp$name == "glasso-ebic"), ],
+    "peak_ram_best",
+    data_uns[!(data_uns$name == "glasso-ebic"), ]) +
+    ggtitle("")
   
   plots_mem_mod[[i]] <- plot1
   plots_mem_bst[[i]] <- plot2
   i <- i + 1
 }
 
-plt_time <- do.call("grid.arrange", c(plots_mem_mod, plots_mem_bst, nrow=2, ncol=5, top="Memory Requirements for Model Estimation and Selection", bottom="Dimension (p)", left="Memory (in MiB)"))
+plt_mem <- do.call("grid.arrange", c(plots_mem_mod, plots_mem_bst, nrow=2, ncol=5, top="Memory Consumption for Model Estimation and Selection", bottom="Dimension (p)", left="Memory (in MiB)"))
 ggsave(
-  plt_time,
+  plt_mem,
   file=paste("./figures/", "memory.pdf", sep = ""),
   width = 22,
+  height = 10,
+  units = "cm")
+
+
+# PLOT HAMMING DIST
+
+
+data_grouped <- add_ci(data_grouped, "hamming_dist")
+
+
+g <- "hub"
+data_tmp <- data_grouped %>%
+  filter(graph == g)
+data_tmp <- na.omit(data_tmp)
+data_uns <- data %>%
+  filter(graph == g)
+data_uns <- na.omit(data_uns)
+plot1 <- plot_models(data_tmp, "hamming_dist", data_uns) +
+  # ylab("Hamming Distance") +
+  coord_cartesian(ylim = c(0, 17500)) +
+ggtitle(get_title(g))
+
+g <- "scale-free"
+data_tmp <- data_grouped %>%
+  filter(graph == g)
+data_tmp <- na.omit(data_tmp)
+data_uns <- data %>%
+  filter(graph == g)
+data_uns <- na.omit(data_uns)
+plot2 <- plot_models(data_tmp, "hamming_dist", data_uns) +
+  # ylab("Hamming Distance") +
+  coord_cartesian(ylim = c(0, 110000)) +
+  ggtitle(get_title(g))
+
+plt <- grid.arrange(
+  plot1,
+  plot2,
+  ncol = 2,
+  left = "Hamming Distance",
+  top = "Hamming Distance",
+  bottom = "Dimension (p)")
+
+ggsave(
+  plt,
+  file=paste("./figures/","hub+scale-free_ham", ".pdf", sep = ""),
+  width = 22,
+  height = 10,
+  units = "cm")
+
+
+
+g <- "band"
+data_tmp <- data_grouped %>%
+  filter(graph == g)
+data_tmp <- na.omit(data_tmp)
+data_uns <- data %>%
+  filter(graph == g)
+data_uns <- na.omit(data_uns)
+plot1 <- plot_models(data_tmp, "hamming_dist", data_uns) +
+  # ylab("Hamming Distance") +
+  coord_cartesian(ylim = c(0, 20000)) +
+  ggtitle(get_title(g))
+
+g <- "cluster"
+data_tmp <- data_grouped %>%
+  filter(graph == g)
+data_tmp <- na.omit(data_tmp)
+data_uns <- data %>%
+  filter(graph == g)
+data_uns <- na.omit(data_uns)
+plot2 <- plot_models(data_tmp, "hamming_dist", data_uns) +
+  # ylab("Hamming Distance") +
+  coord_cartesian(ylim = c(0, 100000)) +
+  ggtitle(get_title(g))
+
+plt <- grid.arrange(
+  plot1,
+  plot2,
+  ncol = 2,
+  left = "Hamming Distance",
+  top = "Hamming Distance",
+  bottom = "Dimension (p)")
+
+ggsave(
+  plt,
+  file=paste("./figures/","band+cluster_ham", ".pdf", sep = ""),
+  width = 22,
+  height = 10,
+  units = "cm")
+
+g <- "MB"
+data_tmp <- data_grouped %>%
+  filter(graph == g)
+data_tmp <- na.omit(data_tmp)
+data_uns <- data %>%
+  filter(graph == g)
+data_uns <- na.omit(data_uns)
+plot1 <- plot_models(data_tmp, "hamming_dist", data_uns) +
+  coord_cartesian(ylim = c(0, 20000)) +
+  ggtitle(get_title(g))
+
+
+ggsave(
+  plot1,
+  file=paste("./figures/","MB_ham", ".pdf", sep = ""),
+  width = 9,
   height = 10,
   units = "cm")
 
@@ -274,6 +420,7 @@ get_metric_name <- function(metric_string) {
   if (metric_string == "f1") return("\\textbf{F1}")
   if (metric_string == "precision") return("\\textbf{Precision}")
   if (metric_string == "recall") return("\\textbf{Recall}")
+  if (metric_string == "hamming_dist") return("\\textbf{Hamming Distance}")
 }
 
 to_table_row <- function(df, col) {
@@ -284,3 +431,14 @@ to_table_row <- function(df, col) {
   written <-apply(X, 1, paste, collapse = " & ")
   cat(paste(written, collapse = "\\\\ \\hline "))
 }
+
+wrap_to_table <- function(df, col, graph) {
+  to_table_row(
+    print_table(df, graph, c(col)),
+    col)}
+
+wrap_to_table(data_grouped, "hamming_dist", "hub")
+wrap_to_table(data_grouped, "hamming_dist", "scale-free")
+wrap_to_table(data_grouped, "hamming_dist", "band")
+wrap_to_table(data_grouped, "hamming_dist", "cluster")
+wrap_to_table(data_grouped, "hamming_dist", "MB")
